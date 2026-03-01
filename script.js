443fca307c692318849d4d17079ab2ebf29debb8
global-ops <lucianoclothes@gmail.com>
2026-03-01 13:55:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -181,8 +181,8 @@ let map; // Сложи го точно тук на нов ред
 
 // --- СЕКЦИЯ 2: ГЕОПОЛИТИЧЕСКИ ДАННИ И ГРАНИЦИ ---
 const warZones = ['Russia', 'Ukraine', 'Syria', 'Sudan', 'Pakistan', 'Afghanistan', 'Iran'];
-const blueZone = ['France', 'Germany', 'United Kingdom', 'Italy', 'Poland', 'Bulgaria', 'Romania', 'Greece', 'Norway', 'Jordan', 'Turkey', 'Saudi Arabia', 'Lithuania', 'Belarus', 'Finland', 'Sweden', 'Qatar', 'Latvia', 'Estonia'];
-const tensionZones = ['North Korea', 'China', 'Taiwan', 'Venezuela', 'USA', 'United States', 'Iraq', 'Yemen', 'Israel', 'Lebanon'];
+const blueZone = ['France', 'Germany', 'United Kingdom', 'Italy', 'Poland', 'Bulgaria', 'Romania', 'Greece', 'Norway', 'Jordan', 'Turkey',  'Lithuania', 'Belarus', 'Finland', 'Sweden', 'Qatar', 'Latvia', 'Estonia'];
+const tensionZones = ['North Korea', 'China', 'Taiwan', 'Venezuela', 'USA', 'United States', 'Iraq', 'Yemen', 'Israel', 'Lebanon', 'Saudi Arabia',];
 
 fetch('https://raw.githubusercontent.com/datasets/geo-boundaries-world-110m/master/countries.geojson')
     .then(res => res.json())
